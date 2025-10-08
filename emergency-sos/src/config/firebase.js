2629a9669ff71cff59ecae293456f7341b07bb0f
Abhinav <abhinavakkumar2004@gmail.com>
2025-10-08 18:03:14 +0530

    Configure login page

--- emergency-sos/src/config/firebase.js
+++ emergency-sos/src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAY4EBSiMRsEGl1Ar9RW-YoHKqsXhXSaTc",
